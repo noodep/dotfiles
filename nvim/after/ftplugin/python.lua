314be7ed1b9b7af2e788e74381b08dd427e06a00
noodep <noodverse@gmail.com>
2023-09-07 10:58:26 -0500

    fix(nvim): adds guard to `python_indent` definition

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -3,9 +3,14 @@
 -- trying to set values here without creating it first would result in trying to index a nil value
 -- see nvim/runtime/autoload/python.vim
 
-vim.g.python_indent = {
-	-- indent closing paren/bracket with the first character of the ligne that started the multiline construct
-	closed_paren_align_last_line = false,
-	-- indent only one 'step' after an open paren/bracket
-	open_paren = 'shiftwidth()',
-}
+-- do not redefine the dictionary if it is already set
+-- since `python_indent` is extended with defaults the first time indentation is required (through autoload)
+-- opening a python file after that would reset `python_indent` to the values below without defaults
+if not vim.g.python_indent then
+	vim.g.python_indent = {
+		-- indent closing paren/bracket with the first character of the ligne that started the multiline construct
+		closed_paren_align_last_line = false,
+		-- indent only one 'step' after an open paren/bracket
+		open_paren = 'shiftwidth()',
+	}
+end
