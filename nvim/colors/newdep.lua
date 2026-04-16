5cdb1839b01d41b85fc26546db89cd608fd63e22
noodep <noodverse@gmail.com>
2026-04-16 13:47:14 -0500

    feat(colorscheme): adds catppuccin based color scheme

--- nvim/colors/newdep.lua
+++ nvim/colors/newdep.lua
@@ -2,43 +2,47 @@ vim.cmd('hi clear')
 if vim.g.syntax_on ~= nil then
 	vim.cmd('syntax reset')
 end
+
 vim.g.colors_name = 'newdep'
 
 -- palette
-local colors = {
-	bg        = '#191c27',
-	fg        = '#aea47a',
-	cursor    = '#92805b',
-	selection = '#172539',
+local mocha = {
+	bg        = '#1e1e2e',
+	fg        = '#cdd6f4',
 
-	black     = '#181818',
-	br_black  = '#343a53',
+	cursor    = '#f5e0dc',
+	selection = '#585b70',
 
-	red       = '#810009',
-	br_red    = '#ac3835',
+	black     = '#45475a',
+	br_black  = '#585b70',
 
-	green     = '#5a844a',
-	br_green  = '#62ac51',
+	red       = '#f38ba8',
+	br_red    = '#f37779',
 
-	yellow    = '#cc8b3f',
-	br_yellow = '#dcdf7c',
+	green     = '#a6e3a1',
+	br_green  = '#89d88b',
 
-	blue      = '#576d8c',
-	br_blue   = '#3097c6',
+	yellow    = '#f9e2af',
+	br_yellow = '#ebd391',
 
-	magenta   = '#724d7c',
-	br_magent = '#d33061',
+	blue      = '#89b4fa',
+	br_blue   = '#74a8fc',
 
-	cyan      = '#5c4f4b',
-	br_cyan   = '#f3dbb2',
+	magenta   = '#f5c2e7',
+	br_magenta = '#f2aede',
 
-	white     = '#aea47f',
-	br_white  = '#f4f4f4',
+	cyan      = '#94e2d5',
+	br_cyan   = '#6bd7ca',
+
+	white     = '#a6adc8',
+	br_white  = '#bac2de',
 }
+
+local colors = mocha
 
 local hls = {
 	-- base UI
-	Normal       = { fg = colors.fg, bg = 'NONE' },
+	Normal       = { fg = colors.fg, bg = 'none' },
 	Visual       = { bg = colors.selection },
 	CursorLine   = { bg = colors.br_black },
 	LineNr       = { fg = colors.blue },
@@ -49,12 +53,12 @@ local hls = {
 	Question     = { fg = colors.br_green },
 
 	-- Syntax
-	Comment      = { fg = colors.br_red, italic = true }, -- Steel blue comments look great on this bg
-	Constant     = { fg = colors.red },
+	Comment      = { fg = colors.red, italic = true },
+	Constant     = { fg = colors.br_red },
 	String       = { fg = colors.br_green },
 	Number       = { fg = colors.br_red },
-	Boolean      = { fg = colors.br_red },
-	Float        = { fg = colors.br_red },
+	Boolean      = { link = Number },
+	Float        = { link = Number },
 
 	Function     = { fg = colors.br_blue },
 	Keyword      = { fg = colors.br_yellow },
@@ -70,7 +74,7 @@ local hls = {
 	Special      = { fg = colors.br_magenta },
 	Todo         = { fg = colors.br_red, bold = true },
 
-	-- Completion Menu (Pmenu) - Using your deep selection blue
+	-- Completion Menu (Pmenu)
 	Pmenu        = { fg = colors.fg, bg = 'none' },
 	PmenuSel     = { fg = colors.br_white, bg = colors.selection, bold = true },
 	PmenuSbar    = { bg = colors.black },
@@ -79,10 +83,10 @@ local hls = {
 	PmenuMatch    = { fg = colors.br_blue, bold = true },
 	PmenuMatchSel = { fg = colors.br_cyan, bold = true },
 	PmenuKind     = { fg = colors.cyan },
-	PmenuExtra    = { fg = colors.comment, italic = true },
+	PmenuExtra    = { fg = colors.br_red, italic = true },
 
 	-- UI Elements
-	SpecialKey   = { fg = colors.cyan },
+	SpecialKey   = { fg = colors.br_black },
 	NonText      = { link = 'SpecialKey' },
 	Whitespace   = { link = 'SpecialKey' },
 	WinSeparator = { fg = colors.br_black, bg = 'none' },
@@ -99,42 +103,10 @@ local hls = {
 	DiagnosticHint  = { fg = colors.br_green },
 
 	-- Gutter/Signs
-	SignColumn   = { bg = 'NONE' },
-	FoldColumn   = { bg = 'NONE', fg = colors.comment },
+	SignColumn   = { bg = 'none' },
+	FoldColumn   = { bg = 'none', fg = colors.br_red },
 }
 
 for group, hl in pairs(hls) do
-	vim.api.nvim_set_hl(0, group, hl)
-end
-
-local modern_hls = {
-	-- Treesitter tokens
-	['@variable']            = { fg = colors.br_white },
-	['@variable.builtin']    = { fg = colors.br_cyan, italic = true },
-	['@constant.builtin']    = { fg = colors.br_red, bold = true },
-	['@function.builtin']    = { fg = colors.br_red, bold = true },
-	['@keyword.import']      = { fg = colors.magenta, italic = true },
-	['@keyword.operator']    = { fg = colors.blue },
-	['@keyword.return']      = { fg = colors.purple, italic = true },
-	['@property']            = { fg = colors.white },
-	['@markup.heading']      = { fg = colors.br_yellow, bold = true },
-	['@markup.link']         = { fg = colors.br_blue },
-	['@punctuation.bracket'] = { fg = colors.fg },
-	['@constructor']         = { fg = colors.green },
-	['@tag']                 = { fg = colors.blue },
-	['@tag.attribute']       = { fg = colors.cyan },
-	['@module']              = { fg = colors.cyan },
-
-	-- LSP Semantic Tokens
-	['@lsp.type.class']         = { link = 'Type' },
-	['@lsp.type.decorator']     = { fg = colors.green },
-	['@lsp.type.constructor']     = { fg = colors.green },
-	['@lsp.type.parameter']     = { fg = colors.fg, italic = true },
-	['@lsp.type.property']      = { fg = colors.br_cyan },
-	['@lsp.type.variable']      = { fg = colors.br_cyan },
-	['@lsp.type.function']      = { fg = colors.br_red, bold = true },
-}
-
-for group, hl in pairs(modern_hls) do
 	vim.api.nvim_set_hl(0, group, hl)
 end
